0b411c2f371ed48f68e24ba8963dbaccab8204ee
michaelMondoro <michaelmondoro@gmail.com>
2022-12-12 23:21:51 -0500

    templatized data from musicstructs.py, updated style and JS to select key

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask import render_template
 
+from musicstructs import *
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', notes=name_to_int_distance.keys())
 
 if __name__ == "__main__":
     app.run(debug=True)
