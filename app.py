43f70391e25f2956ca16e104577e1f1c962e5cd6
michaelMondoro <michaelmondoro@gmail.com>
2022-12-13 21:19:35 -0500

    Added main source and implemented random_seed functionality

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 
 from musicstructs import *
+from pymusicgen import rotate, floatequal, MidiNote, Song, PyMusicGen
 
 app = Flask(__name__)
 
@@ -9,5 +10,12 @@ app = Flask(__name__)
 def home():
     return render_template('index.html', notes=name_to_int_distance.keys())
 
+@app.route("/random_seed")
+def random_seed():
+    musicGen.random_seed()
+    return str(musicGen.seed)
+
+
 if __name__ == "__main__":
+    musicGen = PyMusicGen()
     app.run(debug=True)
